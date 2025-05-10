efdec680e2d479fa13024bc4919492a861ddf0a0
Saathvik k <saathvikkamma@gmail.com>
2025-05-10 13:04:03 -0500

    update site url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://team5427.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
